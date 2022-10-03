047ebe77e660afcd283baa315c262bed4ec0c986
Админ <>
2022-10-03 19:56:08 +0300

    start_files

--- Osipova_2021/Osipova_2021/settings.py
+++ Osipova_2021/Osipova_2021/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello_world'
+    'project'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Osipova_2021.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['Osipova_2021/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
